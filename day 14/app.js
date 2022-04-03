54a1cc43f5c623003f3569bcf1edb3de8667ddc9
ankkyprasad <ankkyprasad@gmail.com>
2022-04-03 23:56:55 +0530

    re-organised

--- day 14/app.js
+++ day 14/app.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const app = express();
 
+require("dotenv").config();
+require("./database/mongoose");
+
+const passport = require("passport");
+
 const PORT = process.env.PORT || 8080;
 
-require("./db/mongoose");
-
 const indexRoutes = require("./routes/index");
-const productRoutes = require("./routes/product");
+const userRoutes = require("./routes/user");
 
+// parsing
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Initializing passport
+app.use(passport.initialize());
+require("./config/passport")(passport);
 
 // routes
 app.use("/", indexRoutes);
-app.use("/product", productRoutes);
+app.use("/user", userRoutes);
 
 app.listen(PORT, (err) => {
   if (err) return console.log(err);
--- day 14/routes/index.js
+++ day 14/routes/index.js
@@ -1,7 +1,19 @@
 const router = require("express").Router();
+const passport = require("passport");
+require("../config/passport")(passport);
 
 router.get("/", (req, res) => {
-  res.send("Hello User");
+  res.send("Hello User!");
 });
+
+// private route only accessed by authorized users
+router.get(
+  "/dashboard",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const message = "Welcome " + req.user.name + ". You're a " + req.user.role;
+    res.json({ title: "success", message });
+  }
+);
 
 module.exports = router;
